0e0d60e2384caeb50c77380a13a50ea00ff89253
Nafay Shaikh <nafaykamran@quranchannel.com>
2021-11-15 15:41:09 +0100

    Implement MassSpringSystemSimulator.cpp

--- Simulations/MassSpringSystemSimulator.cpp
+++ Simulations/MassSpringSystemSimulator.cpp
@@ -1,5 +1,6 @@
 #include "MassSpringSystemSimulator.h"
 
-MassSpringSystemSimulator::MassSpringSystemSimulator(): m_fMass(10), m_fStiffness(40){
+MassSpringSystemSimulator::MassSpringSystemSimulator(): m_fMass(10), m_fStiffness(40), m_fDamping(0), m_fRestLength(1), m_bGravityOn(false), m_vGravity(0,-10,0), m_iIntegrator(EULER){
     
 }
+
