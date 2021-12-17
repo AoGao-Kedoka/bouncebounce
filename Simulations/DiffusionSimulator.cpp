bdf071c8a4d8ce18e8703205d56f59d640659704
Ao Gao <ao.gao@tum.de>
2021-12-17 23:06:30 +0100

    Rebase to kjejdi version and Update UI stuff

--- Simulations/DiffusionSimulator.cpp
+++ Simulations/DiffusionSimulator.cpp
@@ -1,37 +1,61 @@
 #include "DiffusionSimulator.h"
 #include "pcgsolver.h"
+#include <random>
 using namespace std;
 
-Grid::Grid(int m, int n) : _m{ m }, _n{ n } {
-	std::random_device d;
-	std::mt19937 rng(d());
-	std::uniform_real_distribution<> dist(-1, 1);
-	for (int i = 0; i < m; ++i) {
-		std::vector<double> temp;
-		for (int j = 0; j < n; ++j) {
-			if (i == 0 || j == 0 || i == m - 1 || j == n - 1)
-				temp.push_back(0);
-			else {
-				temp.push_back(dist(rng));
-			}
-		}
-		_temperature.push_back(temp);
-	}
+Grid3d::Grid3d(int x, int y, int z) {
+	this->setDimensions(std::make_tuple(x, y, z));
 }
 
-int Grid::getM() { return _m; }
-int Grid::getN() { return _n; }
-float Grid::getTemperature(int l, int w) {
-	if (l < _m || w < _n)
-		return _temperature.at(l).at(w);
-	else
-		throw std::invalid_argument("m or n too big");
+void Grid3d::setDimensions(std::tuple<int, int, int> dim3D) {
+	this->m = std::get<0>(dim3D);
+	this->n = std::get<1>(dim3D);
+	this->p = std::get<2>(dim3D);
+
+	std::random_device rd;
+	std::mt19937 e2(rd());
+	std::uniform_real_distribution<Real> dist(-1.0, 1.0);
+
+
+	this->gridData.resize(m, std::vector<std::vector<Real>>(n, std::vector<Real>(p, 0)));
+
+	for (auto i = 1; i < m - 1; ++i) {
+		// n columns
+		for (auto j = 1; j < n - 1; ++j) {
+			for (auto k = 1; k < p - 1; ++k) {
+				this->gridData[i][j][k] = dist(e2);
+			}
+		}
+	}
+
 }
-void Grid::setTemperatur(int l, int w, double value) {
-	if (l < _m || w < _n)
-		_temperature.at(l).at(w) = value;
-	else
-		throw std::invalid_argument("m or n too big");
+
+
+Grid::Grid(int x, int y) {
+	this->setDimensions(std::make_pair(x, y));
+}
+
+// Set grid dimensions to (x_dimension, y_dimension)
+// Modifies m, n
+void Grid::setDimensions(std::pair<int, int> dim2D) {
+	this->m = dim2D.first;
+	this->n = dim2D.second;
+
+	// Random values in range [-1.0, 1.0]
+	std::random_device rd;
+	std::mt19937 e2(rd());
+	std::uniform_real_distribution<Real> dist(-1.0, 1.0);
+
+	// Initialize MxN matrix with 0 values
+	this->gridData.resize(m, std::vector<Real>(n, 0));
+
+	//set the value of each cell that is not a border
+	for (auto i = 1; i < m - 1; ++i) {
+		// n columns
+		for (auto j = 1; j < n - 1; ++j) {
+			this->gridData[i][j] = dist(e2);
+		}
+	}
 }
 
 
@@ -41,29 +65,25 @@ DiffusionSimulator::DiffusionSimulator()
 	m_vfMovableObjectPos = Vec3();
 	m_vfMovableObjectFinalPos = Vec3();
 	m_vfRotate = Vec3();
-	// to be implemented
-}
-
-DiffusionSimulator::~DiffusionSimulator() {
-	delete(T);
 }
 
 const char* DiffusionSimulator::getTestCasesStr() {
-	return "Explicit_solver, Implicit_solver";
+	return "Explicit_solver, Implicit_solver, 3D_Explicit_Solver, 3D_Implicit_Solver";
 }
 
 void DiffusionSimulator::reset() {
 	m_mouse.x = m_mouse.y = 0;
 	m_trackmouse.x = m_trackmouse.y = 0;
 	m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
-
 }
 
 void DiffusionSimulator::initUI(DrawingUtilitiesClass* DUC)
 {
 	this->DUC = DUC;
-	// to be implemented
-	TwAddVarRW(DUC->g_pTweakBar, "alpha", TW_TYPE_DOUBLE, &this->alpha, "min=0, step=0.2");
+	TwAddVarRW(DUC->g_pTweakBar, "alpha", TW_TYPE_FLOAT, &this->alpha, "min=0.3, max=1, step=0.1");
+	TwAddVarRW(DUC->g_pTweakBar, "m", TW_TYPE_INT8, &this->_m, "min=3");
+	TwAddVarRW(DUC->g_pTweakBar, "n", TW_TYPE_INT8, &this->_n, "min=3");
+	TwAddVarRW(DUC->g_pTweakBar, "p", TW_TYPE_INT8, &this->_p, "min=3");
 }
 
 void DiffusionSimulator::notifyCaseChanged(int testCase)
@@ -71,18 +91,27 @@ void DiffusionSimulator::notifyCaseChanged(int testCase)
 	m_iTestCase = testCase;
 	m_vfMovableObjectPos = Vec3(0, 0, 0);
 	m_vfRotate = Vec3(0, 0, 0);
-	//
-	//to be implemented
-	//
 	switch (m_iTestCase)
 	{
 	case 0:
-		T = new Grid(10, 10);
+		T = new Grid(_m, _n);
+		d3T = nullptr;
 		cout << "Explicit solver!\n";
 		break;
 	case 1:
-		T = new Grid(10, 10);
+		T = new Grid(_m, _n);
+		d3T = nullptr;
 		cout << "Implicit solver!\n";
+		break;
+	case 2:
+		T = nullptr;
+		d3T = new Grid3d(_m, _n, _p);
+		cout << "3D Explicit solver!\n";
+		break;
+	case 3:
+		T = nullptr;
+		d3T = new Grid3d(_m, _n, _p);
+		cout << "3D Implicit solver!\n";
 		break;
 	default:
 		cout << "Empty Test!\n";
@@ -90,63 +119,180 @@ void DiffusionSimulator::notifyCaseChanged(int testCase)
 	}
 }
 
+
+// Returns a new grid calculated from the previous one
 Grid* DiffusionSimulator::diffuseTemperatureExplicit(float timeStep) {//add your own parameters
-	Grid* newT = new Grid(T->getM(), T->getN());
-	for (int i = 0; i < T->getM(); ++i) {
-		for (int j = 0; j < T->getN(); ++j) {
-			if(i == 0 || j == 0 || i == T->getM() - 1 || j == T->getN() - 1){
-				newT->setTemperatur(i, j, T->getTemperature(i, j));
-			} else {
-				/*
-						  d^2u   d^2u
-					t =	 (---- + ---- ) * timeStep * alpha + temperature
-						  dx^2   dy^2
-				*/
-				float centralDiffX = T->getTemperature(i + 1, j) + T->getTemperature(i - 1, j) - 2 * T->getTemperature(i, j);
-				float centralDiffY = T->getTemperature(i, j + 1) + T->getTemperature(i, j - 1) - 2 * T->getTemperature(i, j);
-				newT->setTemperatur(i, j, T->getTemperature(i, j) + alpha * (centralDiffX + centralDiffY) * timeStep);
-			}
+	Grid* newT = new Grid(_m, _n);
+
+	// Name alias
+	std::vector<std::vector<Real>>& data = this->T->gridData;
+
+	// Don't iterate on the border values
+	auto row_len = data.size() - 1;
+	auto column_len = data[0].size() - 1;
+
+	// For each value
+	// Calculate new value in a new time using previous time
+	// Positions used: upperCenter, lowerCenter, center(currentvalue), centerLeft, centerRight
+	// DELTAx = DELTAy = 1
+	for (auto index_row = 1; index_row < row_len; index_row++) {
+		for (auto index_column = 1; index_column < column_len; index_column++) {
+
+			Parameters parameter{ data[index_row + 1][index_column],
+				data[index_row][index_column - 1], data[index_row][index_column], data[index_row][index_column + 1],
+				data[index_row - 1][index_column],1, 1, timeStep };
+
+			newT->gridData[index_row][index_column] = this->updateValuesExplicit(parameter);
 		}
 	}
+
 	return newT;
 }
 
-void setupB(std::vector<Real>& b, Grid* T) {//add your own parameters
-	// to be implemented
+Real DiffusionSimulator::updateValuesExplicit(Parameters& param) {
+	Real new_value = 0;
+
+	Real x_second_deriv = (param.upperCenter - 2 * param.center + param.lowerCenter) / (param.deltaM * param.deltaM);
+	Real y_second_deriv = (param.centerRight - 2 * param.center + param.centerLeft) / (param.deltaN * param.deltaN);
+
+	new_value = param.center + this->alpha * (x_second_deriv + y_second_deriv) * param.timeStep;
+
+	return new_value;
+}
+
+
+Grid3d* DiffusionSimulator::diffuseTemperatureExplicit3d(float timeStep) {
+
+	Grid3d* newT = new Grid3d(_m, _n, _p);
+
+	// Name alias
+	std::vector<std::vector<std::vector<Real>>>& data = this->d3T->gridData;
+
+	// Don't iterate on the border values
+	auto row_len = data.size() - 1;
+	auto column_len = data[0].size() - 1;
+	auto depth_len = data[0][0].size() - 1;
+
+
+	// For each value
+	// Calculate new value in a new time using previous time
+	// Positions used: upperCenter, lowerCenter, center(currentvalue), centerLeft, centerRight
+	// DELTAx = DELTAy= DELTAz = 1
+	for (auto index_row = 1; index_row < row_len; index_row++) {
+		for (auto index_column = 1; index_column < column_len; index_column++) {
+			for (auto index_depth = 1; index_depth < depth_len; index_depth++) {
+				Parameters3d parameter{
+					data[index_row - 1][index_column][index_depth],
+					data[index_row][index_column - 1][index_depth],
+					data[index_row][index_column][index_depth],
+					data[index_row][index_column + 1][index_depth],
+					data[index_row + 1][index_column][index_depth],
+					data[index_row][index_column][index_depth + 1],
+					data[index_row][index_column][index_depth - 1],
+					1, 1, 1, timeStep
+				};
+
+				newT->gridData[index_row][index_column][index_depth] = this->updateValuesExplicit3d(parameter);
+			}
+		}
+	}
+
+	return newT;//leak
+}
+
+Real DiffusionSimulator::updateValuesExplicit3d(Parameters3d& param) {
+	Real new_value = 0;
+
+	Real x_second_deriv = (param.upperCenter - 2 * param.center + param.lowerCenter) / (param.deltaM * param.deltaM);
+	Real y_second_deriv = (param.centerRight - 2 * param.center + param.centerLeft) / (param.deltaN * param.deltaN);
+	Real z_second_deriv = (param.forwardDepth - 2 * param.center + param.backwardDepth) / (param.deltaP * param.deltaP);
+
+	new_value = param.center + this->alpha * (x_second_deriv + y_second_deriv + z_second_deriv) * param.timeStep;
+
+	return new_value;
+}
+
+
+void DiffusionSimulator::setupB(std::vector<Real>& b) {//add your own parameters
+
 	//set vector B[sizeX*sizeY]
-	for (int i = 0; i < T->getM() * T->getN(); i++) {
+	for (int i = 0; i < b.size(); i++) {
 		b.at(i) = 0;
 	}
+
+	std::vector<std::vector<Real>>& data = this->T->gridData;
+
+	for (int i = 0; i < data.size(); ++i) {
+		for (int j = 0; j < data[0].size(); ++j) {
+			b.at(i * data[0].size() + j) = data[i][j];
+		}
+	}
+
 }
 
-void fillT(Grid* T) {//add your own parameters
-	// to be implemented
+void DiffusionSimulator::fillT(std::vector<Real>& x) {//add your own parameters
 	//fill T with solved vector x
 	//make sure that the temperature in boundary cells stays zero
+	std::vector<std::vector<Real>>& data = this->T->gridData;
+
+	const auto rows = data.size();
+	const auto columns = data[0].size();
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			data[i][j] = x.at(i * columns + j);
+			if (i == 0 || j == 0 || (i == rows - 1) || (j == columns - 1)) {
+				data[i][j] = 0;
+			}
+		}
+	}
+
 }
 
-void setupA(SparseMatrix<Real>& A, double factor, Grid* T) {//add your own parameters
-	// to be implemented
+void DiffusionSimulator::setupA(SparseMatrix<Real>& A, double factor) {//add your own parameters
 	//setup Matrix A[sizeX*sizeY*sizeZ, sizeX*sizeY*sizeZ]
 	// set with:  A.set_element( index1, index2 , value );
 	// if needed, read with: A(index1, index2);
 	// avoid zero rows in A -> set the diagonal value for boundary cells to 1.0
-	int l = T->getM() * T->getN();
-	for (int i = 0; i < l; ++i) {
-		A.set_element(i, i, 1); // set diagonal
+
+	std::vector<std::vector<Real>>& data = this->T->gridData;
+	const int N = data.size() * data[0].size();//N = sizeX*sizeY*sizeZ
+
+	const auto rows = data.size();
+	const auto columns = data[0].size();
+
+	A.zero();
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			const auto gridIndex = i * columns + j;
+
+			if (i == 0 || j == 0 || (i == rows - 1) || (j == columns - 1)) {
+				A.set_element(gridIndex, gridIndex, 1);
+			}
+			else {
+				A.set_element(gridIndex, gridIndex, 1 + 4 * factor);
+				A.set_element(gridIndex - 1, gridIndex, -factor);
+				A.set_element(gridIndex + 1, gridIndex, -factor);
+				A.set_element(gridIndex - columns, gridIndex, -factor);
+				A.set_element(gridIndex + columns, gridIndex, -factor);
+			}
+		}
 	}
 }
 
 
 void DiffusionSimulator::diffuseTemperatureImplicit(float timeStep) {//add your own parameters
-	// solve A T = b
-	// to be implemented
-	const int N = T->getN() * T->getM();//N = sizeX*sizeY*sizeZ
+
+	const int N = this->T->gridData.size() * this->T->gridData[0].size();//N = sizeX*sizeY
+
 	SparseMatrix<Real>* A = new SparseMatrix<Real>(N);
 	std::vector<Real>* b = new std::vector<Real>(N);
 
-	setupA(*A, 0.1, T);
-	setupB(*b, T);
+	// setupA(a, factor)
+	// factor is this->alpha * timestep
+	this->setupA(*A, 0.005);
+	setupB(*b);
 
 	// perform solve
 	Real pcg_target_residual = 1e-05;
@@ -163,50 +309,206 @@ void DiffusionSimulator::diffuseTemperatureImplicit(float timeStep) {//add your 
 	// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
 	solver.solve(*A, *b, x, ret_pcg_residual, ret_pcg_iterations, 0);
 	// x contains the new temperature values
-	fillT(T);//copy x to T
+	fillT(x);//copy x to T
+	delete A;
+	delete b;
 }
 
 
 
-void DiffusionSimulator::simulateTimestep(float timeStep)
-{
-	// to be implemented
+void DiffusionSimulator::setupB3d(std::vector<Real>& b) {//add your own parameters
+
+	//set vector B[sizeX*sizeY]
+	for (int i = 0; i < b.size(); i++) {
+		b.at(i) = 0;
+	}
+	std::vector< std::vector<std::vector<Real>>>& data = this->d3T->gridData;
+
+	const auto rows = data.size();
+	const auto columns = data[0].size();
+	const auto depth = data[0][0].size();
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			for (int k = 0; k < depth; ++k) {
+
+				b[i * columns * depth + j * depth + k] = data[i][j][k];
+			}
+		}
+	}
+
+}
+
+void DiffusionSimulator::fillT3d(std::vector<Real>& x) {//add your own parameters
+	//fill T with solved vector x
+	//make sure that the temperature in boundary cells stays zero
+	std::vector< std::vector<std::vector<Real>>>& data = this->d3T->gridData;
+
+	const auto rows = data.size();
+	const auto columns = data[0].size();
+	const auto depth = data[0][0].size();
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			for (int k = 0; k < depth; ++k) {
+
+				if (i == 0 || j == 0 || k == 0 || (i == rows - 1) || (j == columns - 1) || (k == depth - 1)) {
+					data[i][j][k] = 0;
+				}
+				else {
+					data[i][j][k] = x.at(i * columns * depth + j * depth + k);
+				}
+			}
+		}
+	}
+
+}
+
+void DiffusionSimulator::setupA3d(SparseMatrix<Real>& A, double factor) {//add your own parameters
+	//setup Matrix A[sizeX*sizeY*sizeZ, sizeX*sizeY*sizeZ]
+	// set with:  A.set_element( index1, index2 , value );
+	// if needed, read with: A(index1, index2);
+	// avoid zero rows in A -> set the diagonal value for boundary cells to 1.0
+
+	std::vector<std::vector<std::vector<Real>>>& data = this->d3T->gridData;
+
+	const auto rows = data.size();
+	const auto columns = data[0].size();
+	const auto depth = data[0][0].size();
+	A.zero();
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			for (int k = 0; k < depth; ++k) {
+				const auto gridIndex = i * columns * depth + j * depth + k;
+				if (i == 0 || j == 0 || k == 0 || (i == rows - 1) || (j == columns - 1) || (k == depth - 1)) {
+					A.set_element(gridIndex, gridIndex, 1);
+				}
+				else {
+					A.set_element(gridIndex, gridIndex, 1 + 6 * factor);
+					A.set_element(gridIndex - 1, gridIndex, -factor);
+					A.set_element(gridIndex + 1, gridIndex, -factor);
+					A.set_element(gridIndex - columns, gridIndex, -factor);
+					A.set_element(gridIndex + columns, gridIndex, -factor);
+					A.set_element(gridIndex - depth, gridIndex, -factor);
+					A.set_element(gridIndex + depth, gridIndex, -factor);
+				}
+			}
+		}
+	}
+}
+
+void DiffusionSimulator::diffuseTemperatureImplicit3d(float timeStep) {
+
+	const int N = this->d3T->gridData.size() * this->d3T->gridData[0].size() * this->d3T->gridData[0][0].size();//N = sizeX*sizeY*sizeZ
+
+	SparseMatrix<Real>* A = new SparseMatrix<Real>(N);
+	std::vector<Real>* b = new std::vector<Real>(N);
+
+	// setupA(a, factor)
+	// factor is this->alpha * timestep
+	this->setupA3d(*A, 0.01);
+	setupB3d(*b);
+
+	// perform solve
+	Real pcg_target_residual = 1e-05;
+	Real pcg_max_iterations = 1000;
+	Real ret_pcg_residual = 1e10;
+	int  ret_pcg_iterations = -1;
+
+	SparsePCGSolver<Real> solver;
+	solver.set_solver_parameters(pcg_target_residual, pcg_max_iterations, 0.97, 0.25);
+
+	std::vector<Real> x(N);
+	for (int j = 0; j < N; ++j) { x[j] = 0.; }
+
+	// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
+	solver.solve(*A, *b, x, ret_pcg_residual, ret_pcg_iterations, 0);
+	// x contains the new temperature values
+	fillT3d(x);//copy x to T
+	delete A;
+	delete b;
+}
+
+
+
+
+void DiffusionSimulator::simulateTimestep(float timeStep) {
 	// update current setup for each frame
-	switch (m_iTestCase)
-	{
+
+	switch (m_iTestCase) {
 	case 0: {
-		auto temp = T;
+		if (!this->T)
+			this->T = new Grid(_m, _n);
+		Grid* temp = T;
 		T = diffuseTemperatureExplicit(timeStep);
-		delete(temp);
+		delete temp;
 		break;
 	}
 	case 1:
-		diffuseTemperatureImplicit(timeStep);
+		if (!this->T)
+			this->T = new Grid(_m, _n);
+		this->diffuseTemperatureImplicit(timeStep);
+		break;
+	case 2: {
+		if (!this->d3T)
+			this->d3T = new Grid3d(_m, _n, _p);
+		Grid3d* temp3D = d3T;
+		d3T = diffuseTemperatureExplicit3d(timeStep);
+		delete temp3D;
+		break;
+	}
+	case 3:
+		if (!this->d3T)
+			this->d3T = new Grid3d(_m, _n, _p);
+		this->diffuseTemperatureImplicit3d(timeStep);
+		break;
+	default:
+		fprintf(stderr, "No such cases!");
 		break;
 	}
 }
 
 void DiffusionSimulator::drawObjects()
 {
-	// to be implemented
-	//visualization
-	int m = T->getM();
-	int n = T->getN();
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
-				DUC->setUpLighting(Vec3(), Vec3(0, 0, 0), 10, Vec3(0, 0, 0));
+	// 2d visualization
+	if (this->T) {
+		auto& points = this->T->gridData;
+		Real pointSize = 0.05f;
+
+		for (size_t i = 0; i < points.size(); i++) {
+			for (size_t j = 0; j < points[0].size(); j++) {
+
+				DUC->setUpLighting(abs(points[i][j]) * Vec3(1, points[i][j] > 0 ? 1 : 0, points[i][j] > 0 ? 1 : 0),
+					0.4 * Vec3(1, 1, 1),
+					10, abs(points[i][j]) * Vec3(1, 1, 1));
+
+				DUC->drawSphere(Vec3(i / 14.0 - 0.5, j / 14.0 - 0.5, 0), Vec3(pointSize, pointSize, pointSize));
+
 			}
-			else {
-				double t = T->getTemperature(i, j);
-				DUC->setUpLighting(abs(T->getTemperature(i,j)) * 
-									Vec3(1, T->getTemperature(i, j)>0? 1:0, T->getTemperature(i, j) > 0 ? 1 : 0), 
-									0.4 * Vec3(1, 1, 1),10,
-									abs(T->getTemperature(i, j)) * Vec3(1,1,1));
-			}
-			DUC->drawSphere(Vec3(0.05 * (i - m / 2), 0.05 * (j - n / 2), 0), Vec3(0.05, 0.05, 0.05));
 		}
 	}
+	else if (this->d3T) {// 3d
+
+		auto& points = this->d3T->gridData;
+		Real pointSize = 0.05f;
+
+		for (size_t i = 1; i < points.size() - 1; i++) {
+			for (size_t j = 1; j < points[0].size() - 1; j++) {
+				for (size_t k = 1; k < points[0][0].size() - 1; k++) {
+
+					DUC->setUpLighting(abs(points[i][j][k]) * Vec3(1, points[i][j][k] > 0 ? 1 : 0, points[i][j][k] > 0 ? 1 : 0),
+						0.4 * Vec3(1, 1, 1),
+						10, abs(points[i][j][k]) * Vec3(1, 1, 1));
+
+					DUC->drawSphere(Vec3(i / 14.0 - 0.5, j / 14.0 - 0.5, k / 14.0 - 0.5), Vec3(pointSize, pointSize, pointSize));
+				}
+			}
+		}
+
+	}
+
+
 }
 
 
--- Simulations/DiffusionSimulator.h
+++ Simulations/DiffusionSimulator.h
@@ -3,31 +3,79 @@
 
 #include "Simulator.h"
 #include "vectorbase.h"
+#include "pcgsolver.h"
+#include <vector>
+#include <tuple>
+class Grid3d {
+public:
+	// Construtors
+	Grid3d(int x, int y, int z);
+
+	std::vector<std::vector<std::vector<Real>>> gridData;
+	void setDimensions(std::tuple<int, int, int> dim3D);
+private:
+	// Attributes
+	int m;
+	int n;
+	int p;
+};
+
 
 //impement your own grid class for saving grid data
 class Grid {
 public:
 	// Construtors
-	Grid(int m, int n);
-	int getM();
-	int getN();
-	float getTemperature(int l, int w);
-	void setTemperatur(int l, int w, double value);
+	Grid(int x, int y);
 
+	std::vector<std::vector<Real>> gridData;
+	void setDimensions(std::pair<int, int> dim2D);
 private:
 	// Attributes
-	int _m;
-	int _n;
-	std::vector<std::vector<double>> _temperature;
+	int m;
+	int n;
 };
 
+class Parameters {
+public:
+	Parameters(Real upperCenter,Real centerLeft, Real center, Real centerRight, Real lowerCenter,
+		int deltaM, int deltaN, Real timeStep):
+		upperCenter{ upperCenter }, 
+		centerLeft{ centerLeft }, center{ center }, centerRight{ centerRight },
+		lowerCenter{ lowerCenter },
+		deltaM{ deltaM }, deltaN { deltaN }, timeStep{timeStep}{}
 
+	Real upperCenter;
+	Real centerLeft, center, centerRight;
+	Real lowerCenter;
+
+	int deltaM, deltaN; 
+	Real timeStep;
+};
+
+class Parameters3d{
+public:
+	Parameters3d(Real upperCenter, Real centerLeft, Real center, Real centerRight, 
+				Real lowerCenter,Real forwardDepth, Real backwardDepth,
+				int deltaM, int deltaN, int deltaP, Real timeStep):
+		upperCenter{ upperCenter },
+		centerLeft{ centerLeft }, center{ center }, centerRight{ centerRight },
+		lowerCenter{ lowerCenter },
+		forwardDepth{ forwardDepth }, backwardDepth{ backwardDepth },
+		deltaM{ deltaM }, deltaN{ deltaN }, deltaP{ deltaP }, timeStep{ timeStep }{}
+
+	Real upperCenter;
+	Real centerLeft, center, centerRight;
+	Real lowerCenter;
+	Real forwardDepth, backwardDepth;
+
+	int deltaM, deltaN, deltaP; 
+	Real timeStep;
+};
 
 class DiffusionSimulator:public Simulator{
 public:
 	// Construtors
 	DiffusionSimulator();
-	~DiffusionSimulator();
 
 	// Functions
 	const char * getTestCasesStr();
@@ -41,9 +89,25 @@ public:
 	void onMouse(int x, int y);
 	// Specific Functions
 	void drawObjects();
-	Grid* diffuseTemperatureExplicit(float timeStep);
-	void diffuseTemperatureImplicit(float timeStep);
 
+	Real updateValuesExplicit(Parameters& param);
+	Real updateValuesExplicit3d(Parameters3d& param);
+
+	Grid* diffuseTemperatureExplicit(float timeStep);
+	Grid3d* diffuseTemperatureExplicit3d(float timeStep);
+
+	void diffuseTemperatureImplicit(float timeStep);
+	void diffuseTemperatureImplicit3d(float timeStep);
+
+
+	//void assignValues(size_t index_row, size_t index_column, Parameters new_val);
+	void setupA(SparseMatrix<Real>& A, double factor);
+	void setupB(std::vector<Real>& b);
+	void fillT(std::vector<Real>& x);
+
+	void setupA3d(SparseMatrix<Real>& A, double factor);
+	void setupB3d(std::vector<Real>& b);
+	void fillT3d(std::vector<Real>& x);
 private:
 	// Attributes
 	Vec3  m_vfMovableObjectPos;
@@ -52,8 +116,15 @@ private:
 	Point2D m_mouse;
 	Point2D m_trackmouse;
 	Point2D m_oldtrackmouse;
-	Grid *T; //save results of every time step
-	double alpha = 0.3;
+
+	Grid *T = nullptr;
+	Grid3d* d3T = nullptr;
+	
+	int _m = 16;
+	int _n = 16;
+	int _p = 16;
+
+	float alpha = 0.3;//diffusion coef
 };
 
 #endif
