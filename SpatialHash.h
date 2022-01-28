557c8b222c5b930f7f3901cc5d47411f39276812
kSDOT <39572376+kSDOT@users.noreply.github.com>
2022-01-28 10:21:33 +0100

    Add files via upload

---
+++ SpatialHash.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Particle.h"
+//#include "Hasher.h"
+#include <list>
+#include <vector>
+
+class SpatialHash {
+/*SpatialHash(const std::vector<Particle&>& particles, float h);
+	std::list<Particle&> collisions(const Particle& p);
+
+	std::list<Particle&> operator[](const Vec3&);
+	void recalculate();
+private:
+	size_t resize_to_prime(size_t minimum);
+
+
+	std::vector<std::list<Particle&>> hashTable;
+	IHasher* hasher;
+	float h; //smoothing kernel radius
+	*/
+};
